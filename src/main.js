fe04c09d1976fb446efee880127aceb1ddef8307
nabehiro <hiroyuki@commerble.com>
2020-07-25 08:15:46 +0900

    vue add vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import vuetify from './plugins/vuetify';
+import store from './store'
 
 Vue.use(VueAxios, axios)
 
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
